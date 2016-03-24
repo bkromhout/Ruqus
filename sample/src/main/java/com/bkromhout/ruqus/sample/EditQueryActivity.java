61559e18d5ed20f676942cf474e08ae5003a2e9d
Brenden Kromhout <bkromhout@gmail.com>
2016-03-24 00:09:40 -0400

    Sample app

--- sample/src/main/java/com/bkromhout/ruqus/sample/EditQueryActivity.java
+++ sample/src/main/java/com/bkromhout/ruqus/sample/EditQueryActivity.java
@@ -1,13 +1,36 @@
 package com.bkromhout.ruqus.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.bkromhout.ruqus.RealmQueryView;
+import com.bkromhout.ruqus.RealmUserQuery;
 
 public class EditQueryActivity extends AppCompatActivity {
+    @Bind(R.id.rqv)
+    RealmQueryView rqv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setResult(RESULT_CANCELED);
         setContentView(R.layout.activity_edit_query);
+        ButterKnife.bind(this);
+
+        Bundle b = getIntent().getExtras();
+        if (b != null && b.containsKey("RUQ")) rqv.setRealmUserQuery((RealmUserQuery) b.getParcelable("RUQ"));
+    }
+
+    @OnClick(R.id.save)
+    void save() {
+        if (!rqv.isQueryValid()) Toast.makeText(this, R.string.err_invalid_query, Toast.LENGTH_LONG).show();
+        else {
+            setResult(RESULT_OK, new Intent().putExtra("RQV", rqv.getRealmUserQuery()));
+            finish();
+        }
     }
 }
--- sample/src/main/java/com/bkromhout/ruqus/sample/MainActivity.java
+++ sample/src/main/java/com/bkromhout/ruqus/sample/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.bkromhout.ruqus.RealmUserQuery;
 
 public class MainActivity extends AppCompatActivity {
-
+    @Bind(R.id.query)
+    TextView currQuery;
     @Bind(R.id.edit_query)
     Button editQuery;
+
+    RealmUserQuery realmUserQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +25,23 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    private void updateUi() {
+        currQuery.setText(realmUserQuery == null ? "No query." : realmUserQuery.toString());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            realmUserQuery = data.getParcelableExtra("RUQ");
+            updateUi();
+        }
+    }
+
     @OnClick(R.id.edit_query)
     void onEditQClick() {
-        startActivity(new Intent(this, EditQueryActivity.class));
+        Bundle b = new Bundle();
+        b.putParcelable("RUQ", realmUserQuery);
+        startActivityForResult(new Intent(this, EditQueryActivity.class), 1, b);
     }
 }
